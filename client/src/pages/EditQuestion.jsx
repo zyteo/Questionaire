22cea8acdc7774ee29c3a661973ed5426ccc0191
Stabilo <zteo015@e.ntu.edu.sg>
2022-01-09 12:39:14 +0800

    Almost done edit questionaire, left the radio button

--- client/src/pages/EditQuestion.jsx
+++ client/src/pages/EditQuestion.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import styled from "styled-components";
@@ -60,13 +60,129 @@ const Button = styled.button`
   }
   `;
 
-function EditQuestion() {
-  
+function EditQuestion({ userName }) {
+  const navigate = useNavigate();
+  // for the colours checkbox
+  const [checkColors, setCheckColors] = useState({
+    pink: false,
+    red: false,
+    blue: false,
+  });
+  // for all the questions
+  const [questionDetail, setQuestionDetail] = useState({
+    username: userName,
+    fullname: "",
+    colour: "",
+    language: "",
+  });
+  // get question response to populate the questionaire
+  useEffect(() => {
+    async function getQuestionData() {
+      await axios
+        .get(`/api/question/${userName}`)
+        .then((res) => {
+          setQuestionDetail({
+            username: res.data.data.username,
+            fullname: res.data.data.fullname,
+            colour: res.data.data.colour,
+            language: res.data.data.language,
+          });
+          setCheckColors({
+            pink: res.data.data.colour.pink,
+            red: res.data.data.colour.red,
+            blue: res.data.data.colour.blue,
+          });
+        })
+        .catch((err) => console.log(err));
+    }
+    getQuestionData();
+  }, []);
 
+  //for every change in details, update the state
+  const handleNameChange = (event) => {
+    setQuestionDetail({ ...questionDetail, fullname: event.target.value });
+  };
+  const handleRadioChange = (event) => {
+    setQuestionDetail({ ...questionDetail, language: event.target.value });
+  };
+
+  const onChangePink = () => {
+    setCheckColors({ ...checkColors, pink: !checkColors.pink });
+    setQuestionDetail({ ...questionDetail, colour: checkColors });
+  };
+  const onChangeRed = () => {
+    setCheckColors({ ...checkColors, red: !checkColors.red });
+    setQuestionDetail({ ...questionDetail, colour: checkColors });
+  };
+  const onChangeBlue = () => {
+    setCheckColors({ ...checkColors, blue: !checkColors.blue });
+    setQuestionDetail({ ...questionDetail, colour: checkColors });
+  };
+
+  //   on submitting form
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.put(`/api/question/${userName}`, questionDetail).then((res) => {
+      alert(`Update Complete!`);
+      navigate(-1);
+    });
+  };
+
+  // for back button
+  const handleBack = async () => {
+    navigate(-1);
+  };
   return (
     <>
-      <h1>Edit Response</h1>
-      
+      <h1>Questionaire for {userName}</h1>
+      <Label>Full Name:</Label>
+      <Input
+        type="text"
+        name="fullname"
+        minLength="2"
+        value={questionDetail.fullname}
+        onChange={(event) => handleNameChange(event)}
+        required
+      />
+      <br />
+      <Label>Favourite Colour:</Label>
+      <Input
+        type="checkbox"
+        onChange={() => onChangePink()}
+        defaultChecked={questionDetail.colour.pink}
+      />
+      Pink
+      <Input
+        type="checkbox"
+        onChange={() => onChangeRed()}
+        defaultChecked={questionDetail.colour.red}
+      />
+      Red
+      <Input
+        type="checkbox"
+        onChange={() => onChangeBlue()}
+        defaultChecked={questionDetail.colour.blue}
+      />
+      Blue
+      <br />
+      <Label>Preferred Coding Language:</Label>
+      <Input
+        type="radio"
+        name="language"
+        value="Python"
+        onChange={(event) => handleRadioChange(event)}
+      />
+      Python
+      <Input
+        type="radio"
+        name="language"
+        value="Javascript"
+        onChange={(event) => handleRadioChange(event)}
+      />
+      Javascript
+      <Button onClick={(event) => handleSubmit(event)}>Submit</Button>
+      <Button onClick={() => handleBack()}>Back</Button>
     </>
   );
 }
